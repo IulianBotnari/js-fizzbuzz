0fcd48b5932766902d7ed04376adf5319587855a
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-01 17:49:20 +0200

    fix bug 2

--- main.js
+++ main.js
@@ -66,20 +66,33 @@
 
 //another method
 
+// for  (let i = 1; i <= 100; i++){
+//   if (((i%5) == 0) && ((i%3) == 0)) {
+//       console.log("fizzbuzz");
+//     } else if (((i%5) != 0) && ((i%3) != 0))  {
+//       console.log(i);
+//     }
+//   if (((i%3) == 0) && (!(((i%5) == 0) && ((i%3) == 0)))) {
+//     console.log("fizz");
+//     }
+
+//   if (((i%5) == 0) && (!(((i%5) == 0) && ((i%3) == 0)))) {
+//       console.log("buzz");
+//       }
+
+// }
+
+
 for  (let i = 1; i <= 100; i++){
   if (((i%5) == 0) && ((i%3) == 0)) {
       console.log("fizzbuzz");
     } else if (((i%5) != 0) && ((i%3) != 0))  {
       console.log(i);
+    } else if (((i%3) == 0)) {
+      console.log("fizz");
+    } else if (((i%5) == 0)) {
+        console.log("buzz");
     }
-  if (((i%3) == 0) && (!(((i%5) == 0) && ((i%3) == 0)))) {
-    console.log("fizz");
-    }
-
-  if (((i%5) == 0) && (!(((i%5) == 0) && ((i%3) == 0)))) {
-      console.log("buzz");
-      }
-
 }
 
 
